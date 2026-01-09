0e9993a2a2209e2ebf74701afe0899805b85ff96
Thomas Devlamminck <91684310+thomasdevl@users.noreply.github.com>
2026-01-09 18:34:47 +0100

    refactor + parsing

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,16 @@
 #include <vector>
 #include <sys/wait.h>
 
-void run_executable(const std::string& path, const std::string& command, const std::string& args_str) {
+
+std::unordered_set<std::string> builtins{"exit", "echo", "type","pwd", "cd"};
+
+
+
+void run_executable(const std::string& path, const std::string& command, const std::vector<std::string>& arg_list) {
   std::vector<char*> c_args;
 
-  // argv[0] is traditionally the command name itself
+  // argv[0] is the command name itself
   c_args.push_back(const_cast<char*>(command.c_str()));
-
-  // Split args_str by spaces to handle multiple flags (e.g., -l -a)
-  std::stringstream ss(args_str);
-  std::string arg;
-  std::vector<std::string> arg_list; // Keep strings alive in memory
-  while (ss >> arg) {
-    arg_list.push_back(arg);
-  }
 
   for (const auto& a : arg_list) {
     c_args.push_back(const_cast<char*>(a.c_str()));
@@ -35,13 +32,185 @@ void run_executable(const std::string& path, const std::string& command, const s
     // If execv returns, it failed
     perror("execv");
     exit(1);
-  } else if (pid > 0) {
+  }
+
+  if (pid > 0) {
     // Parent process
     int status;
     waitpid(pid, &status, 0);
   } else {
     perror("fork");
   }
+}
+
+void echo(const std::vector<std::string>& arg_list) {
+
+  for (const auto & i : arg_list) {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+
+}
+
+void type(const std::vector<std::string>& arg_list) {
+
+  // should contain the command name
+  const auto& args = arg_list[0];
+
+  if (builtins.contains(args)) {
+    std::cout << args << " is a shell builtin" << std::endl;
+  } else {
+    bool found = false;
+
+    if (const char* env_p = std::getenv("PATH")) {
+      std::stringstream ss(env_p);
+      std::string path_dir;
+
+      while (std::getline(ss, path_dir, ':')) {
+        // Combine directory and command name
+        std::filesystem::path full_path = std::filesystem::path(path_dir) / args;
+
+        if (std::filesystem::exists(full_path)) {
+
+          // check if execute permissions
+          if (!  access(full_path.string().c_str(), X_OK)){
+            std::cout << args << " is " << full_path.string() << std::endl;
+            found = true;
+            break;
+          }
+        }
+      }
+    }
+
+    if (!found) {
+      std::cout << args << ": not found" << std::endl;
+    }
+  }
+}
+
+std::filesystem::path cd(const std::vector<std::string>& arg_list, const std::filesystem::path& curDir) {
+
+  // should contain a path
+  const auto& args = arg_list[0];
+
+  // absolute path
+  if (args[0] == '/') {
+    if (std::filesystem::is_directory(args)) {
+      return args;
+    }
+    std::cout << "cd: " << args << ": No such file or directory" << std::endl;
+
+  } else if (args[0] == '~') {
+    // HOME directory
+    const char* env_h = std::getenv("HOME");
+
+    std::filesystem::path targetDir = std::filesystem::path(env_h);
+
+    if (args.length() > 2){
+      targetDir /= args.substr(2,args.length()-2);
+    }
+
+    if (std::filesystem::is_directory(targetDir)) {
+      return targetDir;
+    }
+    std::cout << "cd: " << targetDir.string() << ": No such file or directory" << std::endl;
+
+  } else if (args.substr(0,3) == "../"){
+    // go up the number of ../ is
+
+    std::filesystem::path targetDir = curDir;
+    std::string tmp_args = args;
+
+    // remove ../ from the front of the string and go up
+    while (tmp_args.substr(0,3) == "../") {
+      targetDir = targetDir.parent_path();
+      tmp_args.erase(0,3);
+    }
+
+    if (!tmp_args.empty()) {
+      targetDir /= tmp_args;
+    }
+
+    // go to the rest of tmp_args
+    if (std::filesystem::is_directory(targetDir)) {
+      return targetDir;
+    } else {
+      std::cout << "cd: " << targetDir.string() << ": No such file or directory" << std::endl;
+    }
+
+  } else {
+    // relative paths
+    auto tmp = curDir;
+
+    if (args.substr(0,2) == "./") {
+      tmp += args.substr(1,args.length());
+    } else {
+      tmp /= args;
+    }
+
+    if (std::filesystem::is_directory(tmp)) {
+      return tmp;
+    }
+    std::cout << "cd: " << tmp.string() << ": No such file or directory" << std::endl;
+  }
+
+  return {};
+}
+
+std::vector<std::string> parse_arguments(const std::string& args) {
+  std::vector<std::string> arg_list;
+  std::string current_arg;
+  char quote_char = '\0';
+  bool escape_next = false;
+
+  for (const char c : args) {
+
+    // Handle escaped character immediately
+    if (escape_next) {
+      if (quote_char == '\"') {
+        if (c != '\"' && c != '\\') {
+          current_arg += '\\';
+        }
+      }
+      current_arg += c;
+      escape_next = false;
+      continue;
+    }
+
+    if (quote_char == '\'') {
+      // INSIDE SINGLE QUOTES: Everything is literal until the next '
+      if (c == '\'') quote_char = '\0';
+      else current_arg += c;
+    }
+    else if (quote_char == '\"') {
+      // INSIDE DOUBLE QUOTES: Watch for \ or closing "
+      if (c == '\\') escape_next = true;
+      else if (c == '\"') quote_char = '\0';
+      else current_arg += c;
+    }
+    else {
+      // OUTSIDE QUOTES
+      if (c == '\\') {
+        escape_next = true;
+      } else if (c == '\'' || c == '\"') {
+        quote_char = c;
+      } else if (std::isspace(static_cast<unsigned char>(c))) {
+        if (!current_arg.empty()) {
+          arg_list.push_back(current_arg);
+          current_arg.clear();
+        }
+      } else {
+        current_arg += c;
+      }
+    }
+  }
+
+  // Capture the final argument
+  if (!current_arg.empty()) {
+    arg_list.push_back(current_arg);
+  }
+
+  return arg_list;
 }
 
 int main() {
@@ -51,9 +220,8 @@ int main() {
   // type : type of file/command/etc
   // pwd : prints working directory
   // cd : change directory
+  // parsing single and double quotes + \
   // + all commands specified in PATH
-
-  std::unordered_set<std::string> builtins{"exit", "echo", "type","pwd", "cd"};
 
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -65,121 +233,35 @@ int main() {
   std::filesystem::path curDir = std::filesystem::current_path();
 
   while (std::getline(std::cin,input)) {
-    std::string delimiter = " ";
-    auto pos = input.find(delimiter);
 
-    std::string command = input.substr(0,pos);
-    std::string args = input.substr(pos+1,input.length());
+    std::vector<std::string> tokens = parse_arguments(input);
+
+    if (tokens.empty()) continue;
+
+    const std::string& command = tokens[0];
+    std::vector arg_list(tokens.begin() + 1, tokens.end());
 
     if (command == "exit") {
       break;
     }
 
     if (command == "echo") {
-      std::cout << args << std::endl;
+      echo(arg_list);
 
     } else if (command == "type") {
+      type(arg_list);
 
-      if (builtins.contains(args)) {
-        std::cout << args << " is a shell builtin" << std::endl;
-      } else {
-        bool found = false;
-        const char* env_p = std::getenv("PATH");
-
-        if (env_p) {
-          std::stringstream ss(env_p);
-          std::string path_dir;
-
-          while (std::getline(ss, path_dir, ':')) {
-            // Combine directory and command name
-            std::filesystem::path full_path = std::filesystem::path(path_dir) / args;
-
-            if (std::filesystem::exists(full_path)) {
-
-              // check if execute permissions
-              if (!  access(full_path.string().c_str(), X_OK)){
-                std::cout << args << " is " << full_path.string() << std::endl;
-                found = true;
-                break;
-              }
-            }
-          }
-        }
-
-        if (!found) {
-          std::cout << args << ": not found" << std::endl;
-        }
-      }
     } else if (command == "pwd") {
       // print working directory
       std::cout << curDir.string() << std::endl;
 
     } else if (command == "cd") {
 
-      // absolute path
-      if (args[0] == '/') {
-        if (std::filesystem::is_directory(args)) {
-          curDir = args;
-        } else {
-          std::cout << "cd: " << args << ": No such file or directory" << std::endl;
-        }
-
-      } else if (args[0] == '~') {
-        // HOME directory
-        const char* env_h = std::getenv("HOME");
-
-        std::filesystem::path targetDir = std::filesystem::path(env_h);
-
-        if (args.length() > 2){
-          targetDir /= args.substr(2,args.length()-2);
-        }
-
-        if (std::filesystem::is_directory(targetDir)) {
-          curDir = targetDir;
-        } else {
-          std::cout << "cd: " << targetDir.string() << ": No such file or directory" << std::endl;
-        }
-
-
-      } else if (args.substr(0,3) == "../"){
-        // go up the number of ../ is
-
-        std::filesystem::path targetDir = curDir;
-        std::string tmp_args = args;
-
-        // remove ../ from the front of the string and go up
-        while (tmp_args.substr(0,3) == "../") {
-          targetDir = targetDir.parent_path();
-          tmp_args.erase(0,3);
-        }
-
-        if (!tmp_args.empty()) {
-          targetDir /= tmp_args;
-        }
-
-        // go to the rest of tmp_args
-        if (std::filesystem::is_directory(targetDir)) {
-            curDir = targetDir;
-        } else {
-            std::cout << "cd: " << targetDir.string() << ": No such file or directory" << std::endl;
-        }
-
-      } else {
-        // relative paths
-        auto tmp = curDir;
-
-        if (args.substr(0,2) == "./") {
-          tmp += args.substr(1,args.length());
-        } else {
-          tmp /= args;
-        }
-
-        if (std::filesystem::is_directory(tmp)) {
-          curDir = tmp;
-        } else {
-          std::cout << "cd: " << tmp.string() << ": No such file or directory" << std::endl;
-        }
+      std::filesystem::path ret = cd(arg_list,curDir);
+      if (!ret.empty()) { // only changes if succeeded
+        curDir = ret;
       }
+
     } else {
 
       // check for implementation in PATH
@@ -198,7 +280,7 @@ int main() {
 
             // check if execute permissions
             if (!  access(full_path.string().c_str(), X_OK)){
-              run_executable(full_path.string(), command, args);
+              run_executable(full_path.string(), command, arg_list);
               found = true;
               break;
             }
